37c7de69310ca04afdbb64f85c9cec57f8d183d3
André Tripode Colognesi <colgnesi.andre@gmail.com>
2019-09-10 14:43:46 -0300

    Testando o sort

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
 include_once "BubbleSort.php";
 $array = array();
@@ -7,60 +17,54 @@ $i = 0;
 do{
     $array[$i] = rand(1,30);
     $i++;
-}while($i < 30);
+}while($i < 100);
 
 $bubble = new BubbleSort($array);
 $bubble->sort();
 $array2 = $bubble->getArray();
+?>
+    <style>
+        .vertical{
+            height: 100%;
+            max-width: 2%;
+            background-color: white;
+            vertical-align: bottom;
 
-foreach($array2 as $i => $a){
-    echo 'novo: '. $array2[$i] . '| antigo: ' .$array[$i] . '<br>';
-}
-
-/*
-
-function buubleSort($array){
-$sortedArray = $array;
-$aux;
-do{
-$swap = false;
-for($i = 0; $i< count($sortedArray); $i++){
-    $next = ($i + 1);
-    if(array_key_exists($next,$sortedArray)){
-        if($sortedArray[$i] > $sortedArray[$next]){
-            $aux = $sortedArray[$next];
-            $sortedArray[$next] = $sortedArray[$i];
-            $sortedArray[$i] = $aux;
-            $swap = true;
         }
-    }
-}
-}while($swap);
-return $sortedArray;
-}
-$sortedArray = buubleSort($array);
+    </style>
+    <div style="height:800px; width:1000px;">
+    <table  style="height:800px; width:1000px; align:bottom">
+    <tr>
+    <?php 
+        foreach($array2 as $a){
+            ?>
+            <td class="vertical">
+                <div style="background-color: black; height:<?=$a?>%; width:100%; align:bottom"></div>
+            </td>
+            <?php
+        }
+    ?>
+    </tr>
+    </table>
+    </div>
 
-for($i = 0; $i < count($sortedArray); $i++){
-    //echo "Antigo: <b>{$array[$i]}</b> | Novo: <b>{$sortedArray[$i]}</b><br>";
-}
+    <div style="height:800px; width:1000px;">
+    <table  style="height:800px; width:1000px; align:bottom">
+    <tr>
+    <?php 
+        foreach($array as $a){
+            ?>
+            <td class="vertical">
+                <div style="background-color: blue; height:<?=$a?>%; width:100%; align:bottom"></div>
+            </td>
+            <?php
+        }
+    ?>
+    </tr>
+    </table>
+    </div>
+  
 
-?>
-
-<?php 
-for($i = 0; $i < count($sortedArray); $i++){
-
-}
-*/
-?>
-<style>
-    .vertical{
-        height: 100px;
-        width:  30px;
-    }
-</style>
-<div class="vertical">
- 
-</div>
-
-
+</body>
+</html>
 
